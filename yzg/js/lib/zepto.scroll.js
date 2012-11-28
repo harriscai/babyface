75936efec9d846624216412c344c75d667989fbd
Leegorous <leegorous@gmail.com>
2012-11-28 16:24:40 +0800

    add zepto

---
+++ yzg/js/lib/zepto.scroll.js
@@ -0,0 +1,46 @@
+;(function($) {
+	var interpolate = function (source, target, shift) { 
+		return (source + (target - source) * shift); 
+	};
+
+	var easing = function (pos) { 
+	    return (-Math.cos(pos * Math.PI) / 2) + .5; 
+	};
+
+	$.scroll = function(endY, duration, easingF, callback) {
+		endY = endY || ($.os.android ? 1 : 0);
+		duration = duration || 200;
+		(typeof easingF === 'function') && (easing = easingF);
+
+		var startY = document.body.scrollTop,
+			startT  = Date.now(),
+			finishT = startT + duration;
+
+		var animate = function() {
+			var now = +(new Date()),
+				shift = (now > finishT) ? 1 : (now - startT) / duration;
+
+			window.scrollTo(0, interpolate(startY, endY, easing(shift)));
+
+			if (now > finishT) {
+				if (callback) callback()
+			} else {
+				setTimeout(animate, 15)
+			}
+			//(now > finishT) || setTimeout(animate, 15);
+		};
+	
+		animate();
+	};
+}(Zepto));
+
+
+
+
+
+
+
+
+
+
+
